de799cf1ac303e2f7e0a9f9c6e471c9f6c7d03a2
Sulakshitha Rathnayake <50855045+bishop7js@users.noreply.github.com>
2025-06-23 15:06:31 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import ClockScreen from "./ClockScreen";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>CLOCK SCREEN</h1>
+      <div>
+        <ClockScreen />
+      </div>
+    </div>
+  );
+}
---
+++ src/ClockScreen.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import "./styles.css";
+
+const ClockScreen = () => {
+  const [now, setNow] = useState({});
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setNow(new Date());
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  const currentHour = now.getHours();
+  const currentMin = now.getMinutes();
+  const currentSecond = now.getSeconds();
+
+  const renderClock = () => {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div>
+          <h1> {currentHour} :</h1>
+        </div>
+        <div>
+          <h1> {currentMin} :</h1>
+        </div>
+        <div>
+          <h1> {currentSecond}</h1>
+        </div>
+      </div>
+    );
+  };
+
+  return <div>{renderClock()}</div>;
+};
+
+export default ClockScreen;
